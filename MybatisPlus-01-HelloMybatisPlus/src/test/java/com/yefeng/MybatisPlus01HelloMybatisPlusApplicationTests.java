ddd14e991d631d6576606c893883ccb8bdb477d9
lengyingmofeng <1797719651@qq.com>
2022-10-09 14:46:14 +0800

    CRUD

--- MybatisPlus-01-HelloMybatisPlus/src/test/java/com/yefeng/MybatisPlus01HelloMybatisPlusApplicationTests.java
+++ MybatisPlus-01-HelloMybatisPlus/src/test/java/com/yefeng/MybatisPlus01HelloMybatisPlusApplicationTests.java
@@ -3,17 +3,29 @@ package com.yefeng;
 import com.yefeng.entity.User;
 import com.yefeng.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 @SpringBootTest
 class MybatisPlus01HelloMybatisPlusApplicationTests {
+    @Autowired
     private UserMapper userMapper;
     @Test
     void contextLoads() {
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
+    }
+
+    @Test
+    void insertUser() {
+        User user = new User();
+        user.setName("百褶");
+        user.setEmail("1652748903@qq.com");
+        int insert = userMapper.insert(user);
+        System.out.println(insert);
+        System.out.println(user);
     }
 
 }
