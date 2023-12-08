5b8c924536f77bd24bda587bad0b96117d82e751
yefeng <1797719651@qq.com>
2023-12-08 15:05:57 +0800

    feat(reflex, mybatis): 将Mybatis提交

--- Mybatis/mybatis-03/src/main/java/com/xxx/Dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xxx.Dao;
-
-import com.xxx.pojo.T_user;
-
-import java.util.List;
-
-public interface UserMapper {
-  List<T_user> getT_user();
-}
--- Mybatis/mybatis-09/src/main/java/com/xxx/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xxx.dao;
-
-import com.xxx.pojo.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-  User queryUserById(@Param("id") int id);
-  int updateUser(User user);
-}
