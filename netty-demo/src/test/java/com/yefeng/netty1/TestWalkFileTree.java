aff1fb69cefc7eb4490b5e64afa8d19d749ef0b2
yefeng <1797719651@qq.com>
2024-02-16 17:47:51 +0800

    Revert "feat(netty-demo): 没想到准备了，同样的事情又在我身上发生"
    This reverts commit 7ad3c9ca96aa14c8813e49cf360dcc019516b356.

--- netty-demo/src/test/java/com/yefeng/netty1/TestWalkFileTree.java
+++ netty-demo/src/test/java/com/yefeng/netty1/TestWalkFileTree.java
@@ -1,36 +1,7 @@
 package com.yefeng.netty1;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class TestWalkFileTree {
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("H:\\TG");
-        // 文件目录数目
-        AtomicInteger dirCount = new AtomicInteger();
-        // 文件数目
-        AtomicInteger fileCount = new AtomicInteger();
-        Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                System.out.println("===>"+dir);
-                // 增加文件目录数
-                dirCount.incrementAndGet();
-                return super.preVisitDirectory(dir, attrs);
-            }
+    public static void main(String[] args) {
 
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                System.out.println(file);
-                // 增加文件数
-                fileCount.incrementAndGet();
-                return super.visitFile(file, attrs);
-            }
-        });
-        // 打印数目
-        System.out.println("文件目录数:"+dirCount.get());
-        System.out.println("文件数:"+fileCount.get());
     }
 }
