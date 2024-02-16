7ad3c9ca96aa14c8813e49cf360dcc019516b356
yefeng <1797719651@qq.com>
2024-02-16 17:39:07 +0800

    feat(netty-demo): 没想到准备了，同样的事情又在我身上发生

--- netty-demo/src/test/java/com/yefeng/netty1/TestWalkFileTree.java
+++ netty-demo/src/test/java/com/yefeng/netty1/TestWalkFileTree.java
@@ -1,7 +1,36 @@
 package com.yefeng.netty1;
 
-public class TestWalkFileTree {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class TestWalkFileTree {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("H:\\TG");
+        // 文件目录数目
+        AtomicInteger dirCount = new AtomicInteger();
+        // 文件数目
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("===>"+dir);
+                // 增加文件目录数
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                // 增加文件数
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+        // 打印数目
+        System.out.println("文件目录数:"+dirCount.get());
+        System.out.println("文件数:"+fileCount.get());
     }
 }
---
+++ netty-demo/src/test/java/com/yefeng/netty2/demo/Client.java
@@ -0,0 +1,14 @@
+package com.yefeng.netty2.demo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress(8080));
+        System.out.println("waiting....");
+    }
+}
