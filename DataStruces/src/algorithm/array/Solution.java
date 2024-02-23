7d8eb2261bff93f35359c8f08dad064db0d88fd7
yefeng <1797719651@qq.com>
2024-02-23 23:50:31 +0800

    feat(algorithm): 旋转数组

---
+++ DataStruces/src/algorithm/array/Solution.java
@@ -0,0 +1,39 @@
+package algorithm.array;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        int[] x = {1, 2, 3};
+        X x1 = new X();
+        x1.replace(x);
+        for (int i : x) {
+            System.out.println(i);
+        }
+    }
+
+    public static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = i; j < matrix.length - 1 - i; j++) {
+                exchange(matrix, i, j, j, matrix.length - 1 - i);
+                exchange(matrix, i, j, matrix.length - 1 - i, matrix.length - 1 - j);
+                exchange(matrix, i, j, matrix.length - 1 - j, i);
+            }
+        }
+    }
+
+    public static void exchange(int[][] matrix, int i1, int j1, int i2, int j2) {
+        matrix[i1][j1] = matrix[i1][j1] + matrix[i2][j2];
+        matrix[i2][j2] = matrix[i1][j1] - matrix[i2][j2];
+        matrix[i1][j1] = matrix[i1][j1] - matrix[i2][j2];
+    }
+}
+
+
+
