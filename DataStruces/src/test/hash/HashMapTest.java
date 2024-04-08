096b6a0b7f9d1dba9d142672070ef4431fd1613b
yefeng <1797719651@qq.com>
2024-04-08 21:12:24 +0800

    feat(DataStr): 查看HashMap源码以及学习HashMap 的 7 种遍历方式

---
+++ DataStruces/src/test/hash/HashMapTest.java
@@ -0,0 +1,60 @@
+package test.hash;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        // 创建并赋值 HashMap
+        Map<Integer, String> map = new HashMap();
+        map.put(1, "Java");
+        map.put(2, "JDK");
+        map.put(3, "Spring Framework");
+        map.put(4, "MyBatis framework");
+        map.put(5, "Java中文社群");
+
+        // 推荐 使用 迭代器 EntrySet
+        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.print(entry.getKey() + "\t");
+            System.out.println(entry.getValue());
+        }
+        System.out.println();
+        // 迭代器 KeySet
+        Iterator<Integer> iterator1 = map.keySet().iterator();
+        while (iterator1.hasNext()) {
+            Integer key = iterator1.next();
+            System.out.print(key + "\t");
+            System.out.println(map.get(key));
+        }
+        System.out.println();
+
+        // ForEach EntrySet
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + "\t");
+            System.out.println(entry.getValue());
+        }
+        System.out.println();
+        // ForEach KeySet遍历
+        for (Integer key : map.keySet()) {
+            System.out.print(key + "\t");
+            System.out.println(map.get(key));
+        }
+        System.out.println();
+
+        // Lambda遍历
+        map.forEach((key, value) -> {
+            System.out.print(key + "\t");
+            System.out.println(value);
+        });
+
+        System.out.println();
+        //Streams API 单线程遍历
+        map.entrySet().stream().forEach((entry) -> {
+            System.out.print(entry.getKey() + "\t");
+            System.out.println(entry.getValue());
+        });
+    }
+}
