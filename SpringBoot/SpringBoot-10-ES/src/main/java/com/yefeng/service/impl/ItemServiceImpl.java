dd438a4d3a2c826a5d41ebf528fc4b5d6b669062
yefeng <1797719651@qq.com>
2024-06-19 22:39:25 +0800

    feat(SpringBoot-10-ES): 使用SpringBoot整合es

--- SpringBoot/SpringBoot-10-ES/src/main/java/com/yefeng/service/impl/ItemServiceImpl.java
+++ SpringBoot/SpringBoot-10-ES/src/main/java/com/yefeng/service/impl/ItemServiceImpl.java
@@ -7,13 +7,7 @@ import com.yefeng.pojo.Item;
 import com.yefeng.service.ItemService;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 商品表 服务实现类
- * </p>
- *
- * @author 虎哥
- */
+
 @Service
 public class ItemServiceImpl extends ServiceImpl<ItemMapper, Item> implements ItemService {
 
