e303bdef935ee06a2b50762a8299fe490dfb645c
yefeng <1797719651@qq.com>
2024-06-24 22:31:05 +0800

    feat(SpringBoot-10-ES): 使用SpringBoot整合es

--- SpringBoot/SpringBoot-10-ES/src/test/java/com/yefeng/DocumentTest.java
+++ SpringBoot/SpringBoot-10-ES/src/test/java/com/yefeng/DocumentTest.java
@@ -14,27 +14,38 @@ import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @Slf4j
 public class DocumentTest {
 
     private RestHighLevelClient client;
-    @Autowired
-    private ItemService itemService;
+
+    @Resource
+    private  ItemService itemService;
 
     @BeforeEach
     void setUp() {
@@ -139,4 +150,154 @@ public class DocumentTest {
             pageNo++;
         }
     }
+
+    @Test
+    void testMatchAll() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testMatch() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testRange() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.rangeQuery("price").gte(10000).lte(30000));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testTerm() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        request.source().query(QueryBuilders.termQuery("brand", "牛奶"));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testBool() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.准备bool查询
+        BoolQueryBuilder bool = QueryBuilders.boolQuery();
+        // 2.2.关键字搜索
+        bool.must(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.3.品牌过滤
+//        bool.filter(QueryBuilders.termQuery("brand", "德亚"));
+        // 2.4.价格过滤
+        bool.filter(QueryBuilders.rangeQuery("price").lte(30000));
+        request.source().query(bool);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+    @Test
+    void testHighlight() throws IOException {
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.query条件
+        request.source().query(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.2.高亮条件
+        request.source().highlighter(
+                SearchSourceBuilder.highlight()
+                        .field("name")
+                        .preTags("<em>")
+                        .postTags("</em>")
+        );
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    private void handleResponse(SearchResponse response) {
+        SearchHits searchHits = response.getHits();
+        // 1.获取总条数
+        long total = searchHits.getTotalHits().value;
+        System.out.println("共搜索到" + total + "条数据");
+        // 2.遍历结果数组
+        SearchHit[] hits = searchHits.getHits();
+        for (SearchHit hit : hits) {
+            // 3.得到_source，也就是原始json文档
+            String source = hit.getSourceAsString();
+            // 4.反序列化
+            itemDoc item = JSONUtil.toBean(source, itemDoc.class);
+            // 5.获取高亮结果
+            Map<String, HighlightField> hfs = hit.getHighlightFields();
+            if (CollUtil.isNotEmpty(hfs)) {
+                // 5.1.有高亮结果，获取name的高亮结果
+                HighlightField hf = hfs.get("name");
+                if (hf != null) {
+                    // 5.2.获取第一个高亮结果片段，就是商品名称的高亮值
+                    String hfName = hf.getFragments()[0].string();
+                    item.setName(hfName);
+                }
+            }
+            System.out.println(item);
+        }
+    }
+
+    @Test
+    void testPageAndSort() throws IOException {
+        int pageNo = 1, pageSize = 5;
+
+        // 1.创建Request
+        SearchRequest request = new SearchRequest("items");
+        // 2.组织请求参数
+        // 2.1.搜索条件参数
+        request.source().query(QueryBuilders.matchQuery("name", "脱脂牛奶"));
+        // 2.2.排序参数
+        request.source().sort("price", SortOrder.ASC);
+        // 2.3.分页参数
+        request.source().from((pageNo - 1) * pageSize).size(pageSize);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+
+//    private void handleResponse(SearchResponse response) {
+//        SearchHits searchHits = response.getHits();
+//        // 1.获取总条数
+//        long total = searchHits.getTotalHits().value;
+//        System.out.println("共搜索到" + total + "条数据");
+//        // 2.遍历结果数组
+//        SearchHit[] hits = searchHits.getHits();
+//        for (SearchHit hit : hits) {
+//            // 3.得到_source，也就是原始json文档
+//            String source = hit.getSourceAsString();
+//            // 4.反序列化并打印
+//            itemDoc item = JSONUtil.toBean(source, itemDoc.class);
+//            System.out.println(item);
+//        }
+//    }
 }
