baaa8d080d20b46582e6762bc2098426faabf5ea
yefeng <1797719651@qq.com>
2024-10-13 23:45:20 +0800

    feat(juc): learn juc

--- juc/src/main/java/com/yefeng/thread/lock/DeadLockDemo.java
+++ juc/src/main/java/com/yefeng/thread/lock/DeadLockDemo.java
@@ -1,8 +1,8 @@
 package com.yefeng.thread.lock;
 
 public class DeadLockDemo {
-    private static Object resource1 = new Object();//资源 1
-    private static Object resource2 = new Object();//资源 2
+    private static final Object resource1 = new Object();//资源 1
+    private static final Object resource2 = new Object();//资源 2
 
     public static void main(String[] args) {
         new Thread(() -> {
@@ -12,7 +12,7 @@ public class DeadLockDemo {
                     // 让线程 A 休眠 1s 为的是让线程 B 得到执行然后获取到 resource2 的监视器锁
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    System.out.println("错误");
                 }
                 System.out.println(Thread.currentThread() + "waiting get resource2");
                 synchronized (resource2) {
