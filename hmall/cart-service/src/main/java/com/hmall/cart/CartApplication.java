e30fe0e65c635185a961674e472adf3d5f3d4409
yefeng <1797719651@qq.com>
2025-01-08 21:10:51 +0800

    :white_check_mark: 配置OpenFeign日志输出

--- hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,12 +1,13 @@
 package com.hmall.cart;
 
+import com.heima.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 指定所在包
-@EnableFeignClients(basePackages = "com.heima.api.client")
+@EnableFeignClients(basePackages = "com.heima.api.client", defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
