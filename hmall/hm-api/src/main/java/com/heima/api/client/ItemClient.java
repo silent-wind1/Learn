2239c56373e02fd8c705b271d4d18ca303a11712
yefeng <1797719651@qq.com>
2025-01-09 23:07:41 +0800

    :white_check_mark: feat(trade): 完成trade模块的拆分

--- hmall/hm-api/src/main/java/com/heima/api/client/ItemClient.java
+++ hmall/hm-api/src/main/java/com/heima/api/client/ItemClient.java
@@ -1,8 +1,11 @@
 package com.heima.api.client;
 
 import com.heima.api.dto.ItemDTO;
+import com.heima.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -13,4 +16,7 @@ public interface ItemClient {
 
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
 }
