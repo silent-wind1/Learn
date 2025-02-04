15fbdbd6b4b2323fdbc3e987ddba9cf53ef6a162
yefeng <1797719651@qq.com>
2025-02-04 14:10:59 +0800

    :white_check_mark: feat(juc): learn write split method

---
+++ juc/src/main/java/com/yefeng/str/StringSplit.java
@@ -0,0 +1,33 @@
+package com.yefeng.str;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: 叶枫
+ * @Date: 2025/02/04/14:07
+ * @Description:
+ */
+public class StringSplit {
+    public static void main(String[] args) {
+        listeningMessageByHeaders("1&&2&&3&&4&&5&");
+    }
+
+    public static void listeningMessageByHeaders(String msg) {
+        String delimiter = "&&";
+        List<String> result = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex = msg.indexOf(delimiter);
+
+        while (endIndex != -1) {
+            result.add(msg.substring(startIndex, endIndex));
+            startIndex = endIndex + delimiter.length();
+            endIndex = msg.indexOf(delimiter, startIndex);
+        }
+
+        result.add(msg.substring(startIndex)); // 添加最后一个部分
+
+        System.out.println(Arrays.toString(result.toArray(new String[0])));
+    }
+}
