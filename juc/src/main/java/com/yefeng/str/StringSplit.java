66513c149d035ad4c68729c281f8715639d4e499
yefeng <1797719651@qq.com>
2025-02-05 13:31:37 +0800

    :white_check_mark: feat(juc): learn write join method

--- juc/src/main/java/com/yefeng/str/StringSplit.java
+++ juc/src/main/java/com/yefeng/str/StringSplit.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class StringSplit {
     public static void main(String[] args) {
-        listeningMessageByHeaders("1&&2&&3&&4&&5&");
+        listeningMessageByHeaders("1&&2&&3&&4&&5&&&3s");
     }
 
     public static void listeningMessageByHeaders(String msg) {
@@ -27,7 +27,17 @@ public class StringSplit {
         }
 
         result.add(msg.substring(startIndex)); // 添加最后一个部分
+        String[] array = result.toArray(new String[0]);
+        System.out.println(Arrays.toString(array));
+        joinString(array);
+    }
 
-        System.out.println(Arrays.toString(result.toArray(new String[0])));
+    public static void joinString(String[] array){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < array.length - 1; i++) {
+            result.append(array[i]).append("&&");
+        }
+        result.append(array[array.length - 1]);
+        System.out.println(result);
     }
 }
