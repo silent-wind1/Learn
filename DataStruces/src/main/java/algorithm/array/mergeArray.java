9daa9ca7879754cceb1a8eb329df1bd9721caf02
yefeng <1797719651@qq.com>
2025-02-11 17:21:40 +0800

    :white_check_mark: feat(data structure): 学习合并两个有序数组

---
+++ DataStruces/src/main/java/algorithm/array/mergeArray.java
@@ -0,0 +1,22 @@
+package main.java.algorithm.array;
+
+import java.util.Arrays;
+
+public class mergeArray {
+    public static void main(String[] args) {
+        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len = m + n - 1;
+        int len1 = m - 1;
+        int len2 = n - 1;
+        while (len1 >= 0 && len2 >= 0) {
+            nums1[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--] : nums2[len2--];
+        }
+
+        for (int i : nums1) {
+            System.out.print(i + "\t");
+        }
+    }
+}
