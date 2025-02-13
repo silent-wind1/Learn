aa16e0c05d850ae13d1c04c9b87b0ae5414bd0c4
yefeng <1797719651@qq.com>
2025-02-13 11:36:56 +0800

    :white_check_mark: feat(data structure): 自主实现数组队列

--- DataStruces/src/main/java/test/queue/ArrayQueueTest.java
+++ DataStruces/src/main/java/test/queue/ArrayQueueTest.java
@@ -5,16 +5,17 @@ import main.java.datastructure.queue.ArrayQueue;
 public class ArrayQueueTest {
     public static void main(String[] args) {
         ArrayQueue<String> queue = new ArrayQueue<>(5);
-        queue.addQueue("田思凯");
-        queue.addQueue("欧阳志飞");
+        queue.addQueue("ak");
+        queue.addQueue("欧阳");
         queue.addQueue("小杨哥");
-        queue.addQueue("唐曦");
+        queue.addQueue("yf");
         for (String s : queue) {
             System.out.println(s);
         }
         System.out.println("队头为：" + queue.getQueue());
         queue.popQueue();
         System.out.println("队头为：" + queue.getQueue());
-
+        int month = Integer.parseInt("01");
+        System.out.println(month);
     }
 }
