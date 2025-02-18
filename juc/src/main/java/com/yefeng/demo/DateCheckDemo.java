1363f0c3fefafbc4cf74a49f3b9c8e7ca9ff01cd
yefeng <1797719651@qq.com>
2025-02-18 18:07:12 +0800

    :white_check_mark: 测试DateCheckDemo

---
+++ juc/src/main/java/com/yefeng/demo/DateCheckDemo.java
@@ -0,0 +1,20 @@
+package com.yefeng.demo;
+
+import java.time.LocalDate;
+
+public class DateCheckDemo {
+    public static void main(String[] args) {
+        // 获取当前日期
+        LocalDate currentDate = LocalDate.now();
+
+        // 获取当前月的15号
+        LocalDate fifteenthDay = currentDate.withDayOfMonth(15);
+
+        // 判断当前日期是否在15号之前
+        if (currentDate.isBefore(fifteenthDay)) {
+            System.out.println("当前日期是15号之前");
+        } else {
+            System.out.println("当前日期是15号之后");
+        }
+    }
+}
