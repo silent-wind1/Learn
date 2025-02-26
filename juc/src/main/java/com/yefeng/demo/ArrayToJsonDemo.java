9bf356d6c94f20cc958f85e9a7f3584f656fb54e
yefeng <1797719651@qq.com>
2025-02-26 18:37:34 +0800

    :white_check_mark: feat: 学习字符串转换成JSON对象

--- juc/src/main/java/com/yefeng/demo/ArrayToJsonDemo.java
+++ juc/src/main/java/com/yefeng/demo/ArrayToJsonDemo.java
@@ -3,12 +3,32 @@ package com.yefeng.demo;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayToJsonDemo {
     public static void main(String[] args) {
-        String arrayImg = "[\"F_1894687464601227264\",\"F_1894678794367328256\",\"F_1894681772188475392\",\"F_1894687464345374720\",\"F_1894687464475398144\"]";
-        JSONArray objects = JSONUtil.parseArray(arrayImg);
-        for (Object object : objects) {
-            System.out.println(object.toString());
+        String invoiceIds = "[\"F_1894687464601227264\",\"F_1894678794367328256\",\"F_1894681772188475392\",\"F_1894687464345374720\",\"F_1894687464475398144\"]";
+        JSONArray objects = JSONUtil.parseArray(invoiceIds);
+        List<String> news = new ArrayList<>();
+        List<String> olds = new ArrayList<>();
+        // F_开头表示旧图片id
+        for (Object s : objects) {
+            String value = s.toString();
+            if (value.startsWith("F_")) {
+                olds.add(value.replace("F_", ""));
+            } else {
+                news.add(value);
+            }
+        }
+        List<String> list = new ArrayList<>();
+        list.add("1894687464601227264");
+        list.add("1894678794367328256");
+
+        for (String tinvoiceFile : list) {
+            if(!olds.contains(tinvoiceFile)) {
+                System.out.println("进行删除操作");
+            }
         }
     }
 }
