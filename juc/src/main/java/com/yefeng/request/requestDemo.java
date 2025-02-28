d3f57b1ebacd0e8dd4854c03d397280b9c90e3a5
yefeng <1797719651@qq.com>
2025-02-28 14:18:23 +0800

    :white_check_mark: feat: 测试数据

--- juc/src/main/java/com/yefeng/request/requestDemo.java
+++ juc/src/main/java/com/yefeng/request/requestDemo.java
@@ -6,12 +6,14 @@ import cn.hutool.json.JSONUtil;
 
 public class requestDemo {
     public static void main(String[] args) {
-        String url = "http://rpa.bosc-platform.com:8888/rpa/getMsg/123?type=code";
+        String url = "http://rpa.bosc-platform.com:8888/rpa/taxApply/123" + "?code=" + "code";
         String response = HttpUtil.get(url);
 //        response = null;
         JSONObject jsonObject = JSONUtil.parseObj(response);
         if(jsonObject.isEmpty()) {
             System.out.println("json 为空");
+        } else {
+            System.out.println(jsonObject.get("code"));
         }
         if (response == null) {
             System.out.println("请求失败");
