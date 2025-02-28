6a655ae98979e1caec3482827ad9c98cf8830f39
yefeng <1797719651@qq.com>
2025-02-28 14:18:23 +0800

    :white_check_mark: feat: test mybatis plus

---
+++ Mybaits_Plus/src/main/java/com/yefeng/controller/userController.java
@@ -0,0 +1,28 @@
+package com.yefeng.controller;
+
+import com.yefeng.entity.User;
+import com.yefeng.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author: 叶枫
+ * @Date: 2025/03/02/21:20
+ * @Description:
+ */
+@Slf4j
+@RestController("/user")
+@RequiredArgsConstructor
+public class userController {
+
+    private final UserService userService;
+
+    @GetMapping("/list")
+    public List<User> getUsers() {
+        return userService.list();
+    }
+}
