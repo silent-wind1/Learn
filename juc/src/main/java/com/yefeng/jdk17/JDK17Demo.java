43603a51716a92c24ba773c3cfa4929ab212779c
yefeng <1797719651@qq.com>
2025-03-06 23:15:29 +0800

    :white_check_mark: feat: 学习JDK17新特性

--- juc/src/main/java/com/yefeng/jdk17/JDK17Demo.java
+++ juc/src/main/java/com/yefeng/jdk17/JDK17Demo.java
@@ -8,23 +8,25 @@ package com.yefeng.jdk17;
 public class JDK17Demo {
     public static void main(String[] args) {
         String name = "JDK17Demo";
-        formmatter(name);
-
-        Object obj = "Hello, Java 17!";
-
-        // 使用 JDK 17 语法的 instanceof 模式匹配
-        if (obj instanceof String s) {
-            // s 直接作为 String 类型变量使用
-            System.out.println("字符串长度: " + s.length());
-        } else {
-            System.out.println("不是字符串类型");
-        }
+        formater(name);
+        switchFormater(name);
     }
 
-    private static void formmatter(Object name) {
+    private static void switchFormater(Object obj) {
+        String result = switch (obj) {
+            case Integer i -> "整数: " + i;
+            case String s  -> "字符串: " + s;
+            default        -> "未知类型";
+        };
+        System.out.println(result);
+    }
+
+    private static void formater(Object name) {
         String message = "Hello";
         if (name instanceof String s) {
             message = String.format("%s %s", message, name);
+            System.out.println("s的长度 = " + s.length());
+            System.out.println("s的名字 = " + s);
         }
         System.out.println(message);
     }
