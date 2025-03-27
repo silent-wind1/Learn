64049c280b8d1914ba2c7965125ea788a015a6e6
yefeng <1797719651@qq.com>
2025-03-27 22:55:58 +0800

    feat(juc): 查询合伙人，服务商，运营商是否都通过审核，如果都通过，则更新通过状态

--- juc/src/main/java/com/yefeng/str/StringDemo.java
+++ juc/src/main/java/com/yefeng/str/StringDemo.java
@@ -2,6 +2,9 @@ package com.yefeng.str;
 
 import cn.hutool.core.util.StrUtil;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author: 叶枫
  * @Date: 2025/03/24/21:03
@@ -16,6 +19,14 @@ public class StringDemo {
             System.out.println("营业执照和信用代码必须同时存在或同时为空");
         } else {
             System.out.println("营业执照和信用代码校验通过");
+        }
+        // 查询合伙人，服务商，运营商是否都通过审核，如果都通过，则更新通过状态
+        List<String> statuses = Arrays.asList("2", "2", "1");
+        long countApproved = statuses.stream().filter("2"::equals).count();
+        if (countApproved == 3) {
+            System.out.println("审核通过");
+        } else if (countApproved == 2 && statuses.contains("1")) {
+            System.out.println("审核中");
         }
     }
 }
