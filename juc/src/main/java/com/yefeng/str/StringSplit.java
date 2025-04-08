cd05ef7b6c40e535cca62a78909b01f66e1968c1
yefeng <1797719651@qq.com>
2025-04-08 22:39:48 +0800

    feat: 新增生成一个带有指定前缀的随机数字序列

--- juc/src/main/java/com/yefeng/str/StringSplit.java
+++ juc/src/main/java/com/yefeng/str/StringSplit.java
@@ -1,8 +1,11 @@
 package com.yefeng.str;
 
+import cn.hutool.core.lang.id.NanoId;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
@@ -13,6 +16,8 @@ import java.util.stream.Collectors;
 public class StringSplit {
     public static void main(String[] args) {
         splitString("1&&2&&3&&4&&5&&&3s");
+        String str = createNumberSeq("B", 20);
+        System.out.println(str);
     }
 
     public static void splitString(String msg) {
@@ -48,5 +53,15 @@ public class StringSplit {
 
     public static void joinString3(String[] array) {
         System.out.println(String.join("&&", array));
+    }
+
+    /**
+     * 该方法用于生成一个带有指定前缀的随机数字序列
+     * @param prefix 指定前缀
+     * @param size 指定序列长度
+     * @return 带有指定前缀的随机数字序列
+     */
+    public static String createNumberSeq(String prefix, int size) {
+        return prefix + NanoId.randomNanoId(new Random(), "0123456789".toCharArray(), size);
     }
 }
