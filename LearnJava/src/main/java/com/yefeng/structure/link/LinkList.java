eb979220a73597df10719d713050549d6f3577cb
yefeng <1797719651@qq.com>
2025-04-22 19:19:07 +0800

    :art: reference: 学习数据结构链表，完成合并两个有序链表方法

---
+++ LearnJava/src/main/java/com/yefeng/structure/link/LinkList.java
@@ -0,0 +1,140 @@
+package com.yefeng.structure.link;
+
+import java.util.Iterator;
+
+/**
+ * @Author: 叶枫
+ * @Date: 2025/04/22/18:12
+ * @Description:
+ */
+public class LinkList<T extends Comparable<T>> implements Iterable<T> {
+    // 头节点
+    private final Node head;
+    // 链表数量
+    private int N;
+
+    /**
+     * 内部类
+     */
+    private class Node {
+        T data;
+        Node next;
+        public Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public LinkList() {
+        this.head = new Node(null, null);
+        this.N = 0;
+    }
+
+    /**
+     * 清空链表
+     */
+    public void clear() {
+        // 只需要把头结点next指向null
+        head.next = null;
+        this.N = 0;
+    }
+
+    /**
+     * 判断链表是否为空
+     * @return 为空返回ture，负责返回false
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    /**
+     * 链表长度
+     * @return 返回链表长度
+     */
+    public int size() {
+        return N;
+    }
+
+    /**
+     * 添加元素
+     * @param data 元素
+     */
+    public void add(T data) {
+        // 把头节点复制给一个新元素
+        Node node = head;
+        // 循环遍历找到尾结点
+        while (node.next != null) {
+            node = node.next;
+        }
+        //
+        node.next = new Node(data, null);
+        this.N++;
+    }
+
+    /**
+     * 合并两个有序链表，前提是两个链表里面的值都是有序的
+     * @param other 第二个链表
+     * @return 合并完的有序链表
+     */
+    public LinkList<T> merge(LinkList<T> other) {
+        if(other == null || other.isEmpty()) {
+            return this;
+        }
+        if(this.isEmpty()) {
+            return other;
+        }
+        LinkList<T> result = new LinkList<>();
+        Node p1 = this.head.next;
+        Node p2 = other.head.next;
+        // 保存要返回的链表头节点
+        Node p3 = result.head;
+        // 如果p1和p2链表都不为空
+        while (p1 != null && p2 != null) {
+            // 比较p1.data和p2.data谁打谁小
+            if(p1.data.compareTo(p2.data) <= 0) {
+                // 如果成立p3指向p1.data
+                p3.next = new Node(p1.data, null);
+                p1 = p1.next;
+            } else {
+                p3.next = new Node(p2.data, null);
+                p2 = p2.next;
+            }
+            p3 = p3.next;
+            result.N++;
+        }
+        // 可能存在p1没有循环完
+        while(p1 != null) {
+            p3.next = new Node(p1.data, null);
+            p1 = p1.next;
+            p3 = p3.next;
+            result.N++;
+        }
+
+        // 可能存在p2没有循环完
+        while(p2 != null) {
+            p3.next = new Node(p2.data, null);
+            p2 = p2.next;
+            p3 = p3.next;
+            result.N++;
+        }
+
+        return result;
+    }
+
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node current = head.next;
+
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+}
---
+++ LearnJava/src/main/java/com/yefeng/structure/link/LinkTest.java
@@ -0,0 +1,22 @@
+package com.yefeng.structure.link;
+
+/**
+ * @Author: 叶枫
+ * @Date: 2025/04/22/19:10
+ * @Description:
+ */
+public class LinkTest {
+    public static void main(String[] args) {
+        LinkList<Integer> linkList1 = new LinkList<>();
+        linkList1.add(1);
+        linkList1.add(3);
+        LinkList<Integer> linkList2 = new LinkList<>();
+        linkList2.add(2);
+        linkList2.add(4);
+        linkList2.add(5);
+        LinkList<Integer> merge = linkList1.merge(linkList2);
+        for (Integer data : merge) {
+            System.out.print(data + "\t");
+        }
+    }
+}
