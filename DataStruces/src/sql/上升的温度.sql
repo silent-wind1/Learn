27ca87cc4b091b15edc5bb465495aa88a1c57d71
yefeng <silend-wind@qq.com>
2025-07-29 12:01:39 +0800

    feat: study SQL 上升温度题目
    - 通过datediff函数计算两个日期之间的天数差

---
+++ DataStruces/src/sql/上升的温度.sql
@@ -0,0 +1,31 @@
+create table weather
+(
+    id          int primary key auto_increment comment '主键',
+    record_date  date comment '日期',
+    temperature int comment '温度'
+) comment '天气';
+
+
+insert into weather
+values (1, '2025-07-01', 10),
+       (2, '2025-07-02', 25),
+       (3, '2025-07-03', 20),
+       (4, '2025-07-04', 30),
+       (5, '2025-07-05', 40),
+       (6, '2025-07-06', 35),
+       (7, '2025-07-27', 35),
+       (8, '2025-07-28', 25),
+       (9, '2025-07-29', 35);
+
+select a.id
+from Weather as a,
+     Weather as b
+where datediff(a.record_date, b.record_date) = 1
+  and a.temperature > b.temperature;
+
+-- datediff() 函数 计算两个日期之间的天数差
+select a.id, a.temperature, a.record_date
+from Weather as a
+         inner join Weather as b
+where datediff(a.record_date, b.record_date) = 1
+  and a.temperature > b.temperature;
