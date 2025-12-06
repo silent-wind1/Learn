69db4158abe18beb883b740df12525df9a23c4f2
yefeng <silend-wind@qq.com>
2025-12-06 17:34:30 +0800

    :mute: feat: 实现EasyExcel文件读取和写入，解决如何处理无法指定行数写入问题

--- LearnJava/src/main/java/com/yefeng/excel/DataDemo.java
+++ LearnJava/src/main/java/com/yefeng/excel/DataDemo.java
@@ -11,20 +11,12 @@ import lombok.Data;
  */
 @Data
 public class DataDemo {
-    @ExcelProperty(value = "序号", index = 0)
     private String id;
-    @ExcelProperty(value = "姓名", index = 1)
     private String name;
-    @ExcelProperty(value = {"公积金", "总计"})
     private String socialTotal;
-    @ExcelProperty(value = {"公积金", "企业"})
     private String socialCompany;
-    @ExcelProperty(value = {"公积金", "个人"})
     private String socialPerson;
-    @ExcelProperty(value = {"养老", "总计"})
     private String ylTotal;
-    @ExcelProperty(value = {"养老", "企业"})
     private String ylCompany;
-    @ExcelProperty(value = {"养老", "个人"})
     private String ylPerson;
 }
--- LearnJava/src/main/java/com/yefeng/excel/ExcelDemo.java
+++ LearnJava/src/main/java/com/yefeng/excel/ExcelDemo.java
@@ -1,7 +1,10 @@
 package com.yefeng.excel;
 
 import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.enums.CellExtraTypeEnum;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+
+import static com.yefeng.excel.DataDemoListener.cachedDataList;
 
 /**
  * @author wind
@@ -9,18 +12,23 @@ import com.alibaba.excel.enums.CellExtraTypeEnum;
  * @date 2025/12/4 19:05
  */
 public class ExcelDemo {
+
     public static void main(String[] args) {
-        // 写法1：JDK8+ ,不用额外写一个DemoDataListener
-        // since: 3.0.0-beta1
         String fileName = "./社保.xlsx";
-        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-        // 指定表头行数为2（因为Excel有两行表头）
-        EasyExcel.read(fileName, DataDemo.class, new DataDemoListener())
-                // 需要读取合并单元格信息 默认不读取
-                .extraRead(CellExtraTypeEnum.MERGE)
-                .sheet()
-                .doRead();
+        EasyExcel.read(fileName, DataDemo.class, new DataDemoListener()).headRowNumber(2).sheet().doRead();
+
+        String name = "./社保1.xlsx";
+        String templateFileName = "./模板.xlsx";
+
+        // 使用模板文件写入，会保留模板的前3行，从第4行开始追加数据
+        ExcelWriter excelWriter = EasyExcel.write(name, DataDemo.class).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().needHead(false).build();
+
+        // 写入数据，会从模板文件的下一行开始（第4行）
+        excelWriter.write(cachedDataList, writeSheet);
+
+        // 关闭流
+        excelWriter.finish();
 
     }
 }
