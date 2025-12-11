ee4156477d5543183d17d4867b63f5d3e6b3bad1
yefeng <silend-wind@qq.com>
2025-12-11 23:15:51 +0800

    :mute: feat: 去除无效导入包

--- LearnJava/src/main/java/com/yefeng/excel/ExcelDemo.java
+++ LearnJava/src/main/java/com/yefeng/excel/ExcelDemo.java
@@ -1,15 +1,9 @@
 package com.yefeng.excel;
 
-import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.write.metadata.WriteSheet;
-
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import static com.yefeng.excel.DataDemoListener.cachedDataList;
 
 /**
  * @author wind
@@ -35,12 +29,12 @@ public class ExcelDemo {
         String now = YearMonth.now().format(DateTimeFormatter.ofPattern("yyyyMM"));
         System.out.println(now);
 
-        String path = String.format("D:/各地区社保公积金数据表/%s/附件/深圳-深圳市同仁科技实业有限公司-社保缴费明细.xlsx", date );
+        String path = String.format("D:/各地区社保公积金数据表/%s/附件/深圳-深圳市同仁科技实业有限公司-社保缴费明细.xlsx", date);
         System.out.println(path);
         String socialPath = "D:/各地区社保公积金数据表/202512/附件/深圳-深圳市同仁科技实业有限公司-社保缴费明细.xlsx";
         String fileName = socialPath.substring(socialPath.lastIndexOf("/") + 1);
         String[] name = fileName.split("-");
-        if(SPECIAL_EXCEL.stream().anyMatch(name[name.length - 1]::equals)) {
+        if (SPECIAL_EXCEL.stream().anyMatch(name[name.length - 1]::equals)) {
             System.out.println(name[name.length - 1]);
         }
 
