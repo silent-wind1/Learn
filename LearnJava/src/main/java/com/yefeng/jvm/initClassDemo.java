730d8b6f5304caf64e55d429903438cc0cc368aa
yefeng <silend-wind@qq.com>
2025-12-14 21:47:51 +0800

    :mute: feat: 学习查看字节码

--- LearnJava/src/main/java/com/yefeng/jvm/initClassDemo.java
+++ LearnJava/src/main/java/com/yefeng/jvm/initClassDemo.java
@@ -13,6 +13,19 @@ public class initClassDemo {
     }
 
     public static void main(String[] args) {
+        /**
+         *  0 bipush 24
+         *  2 putstatic #13 <com/yefeng/jvm/initClassDemo.value : I>
+         *  5 bipush 25
+         *  7 putstatic #13 <com/yefeng/jvm/initClassDemo.value : I>
+         *  10 return
+         *
+         * 1.加载initClassDemo类
+         * 2.执行initClassDemo类的static块，将value赋值为25
+         * 3.执行main方法，将value赋值为24
+         * 4.输出value的值
+         * 5.程序结束
+         */
         System.out.println(initClassDemo.value);
     }
 }
