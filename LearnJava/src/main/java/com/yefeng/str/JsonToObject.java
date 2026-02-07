924fa8578091ca4c8696e999c3da419db594930c
yefeng <silend-wind@qq.com>
2026-02-07 22:35:00 +0800

    :mute: feat: 添加一个新功能

--- LearnJava/src/main/java/com/yefeng/str/JsonToObject.java
+++ LearnJava/src/main/java/com/yefeng/str/JsonToObject.java
@@ -1,10 +1,15 @@
 package com.yefeng.str;
 
+import cn.hutool.json.JSONUtil;
+import com.yefeng.demo.study.dto.GoodItems;
+
 /**
  * Json转换成对象
  */
 public class JsonToObject {
     public static void main(String[] args) {
-
+        String json = "{\"goodId\":\"123456\"}";
+        GoodItems goodItems = JSONUtil.toBean(json, GoodItems.class);
+        System.out.println(goodItems.getGoodId());
     }
 }
